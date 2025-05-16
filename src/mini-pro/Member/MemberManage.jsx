d9328f286c4a2f7030a6c4f50454fb03fd2b6e95
chocozzang <jangjh_05@naver.com>
2025-05-16 22:25:59 +0900

    회원관리페이지

--- src/mini-pro/Member/MemberManage.jsx
+++ src/mini-pro/Member/MemberManage.jsx
@@ -1,14 +1,76 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 const MemberPage = styled.div`
     width : 800px;
+    margin: 40px auto;
+    background-color: #fff;
+    padding: 24px;
+    border-radius: 12px;
+    box-shadow: 0 2px 10px rgba(0,0,0,0.05);
+    font-family: sans-serif;
+`
+
+const Member = styled.div`
+    width : 800px;
     margin : 0 auto;
+    display : flex;
+    flex-direction : row;
 `
 
 function MemberManage() {
+
+    const fetched = useRef(false);
+    const [ userList, setUserList ] = useState();
+    async function getUserList() {
+        const token = localStorage.getItem("JWTtoken");
+        const users = await axios.get("http://localhost:8080/auth/userlist", {
+                headers : {
+                    "Authorization" : `Bearer ${token}`,
+                    "Content-Type" : "application/json",
+                },
+                withCredentials : true});
+        setUserList(users.data);
+        console.log(users);
+    }
+
+
+    useEffect(() => {
+        if(fetched.current) return;
+        else fetched.current = true;
+
+        getUserList();
+    })
     return (
-        <MemberPage>MemberManage</MemberPage>
+        <MemberPage>
+            <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Role</th>
+                <th className="actions">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+             {
+             userList ? 
+             (
+                userList.map((user) => (
+                    <tr key={user.userId}>
+                        <td>{user.userId}</td>
+                        <td>{user.username}</td>
+                        <td>{user.role}</td>
+                        <td className="actions">
+                            <button>탈퇴</button>
+                        </td>
+                    </tr>
+                ) )
+            ) : <></>}
+            </tbody>
+            </table>
+        </MemberPage>
     )
 }
 
