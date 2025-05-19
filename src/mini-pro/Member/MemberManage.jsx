dec12661b66896a007a04a1c29d611ff45ec5155
chocozzang <jangjh_05@naver.com>
2025-05-19 20:17:43 +0900

    user manage page

--- src/mini-pro/Member/MemberManage.jsx
+++ src/mini-pro/Member/MemberManage.jsx
@@ -4,19 +4,13 @@ import styled from 'styled-components';
 
 const MemberPage = styled.div`
     width : 800px;
-    margin: 40px auto;
+    margin: 20px auto;
     background-color: #fff;
     padding: 24px;
     border-radius: 12px;
     box-shadow: 0 2px 10px rgba(0,0,0,0.05);
     font-family: sans-serif;
-`
-
-const Member = styled.div`
-    width : 800px;
-    margin : 0 auto;
-    display : flex;
-    flex-direction : row;
+    justify-content : center;
 `
 
 function MemberManage() {
@@ -35,6 +29,22 @@ function MemberManage() {
         console.log(users);
     }
 
+    async function deleteUser(uid) {
+        const token = localStorage.getItem("JWTtoken");
+        await axios.delete(`http://localhost:8080/auth/deleteUser/${uid}`, {
+            headers : {
+                "Authorization" : `Bearer ${token}`,
+                "Content-Type" : "application/json",
+            },
+            withCredentials : true
+        })
+        .then(() => {
+            let updateUser = userList.filter((user) => user.userId !== uid);
+            setUserList(updateUser);
+        })
+        .catch(e => console.log(e));
+    }
+
 
     useEffect(() => {
         if(fetched.current) return;
@@ -44,13 +54,14 @@ function MemberManage() {
     })
     return (
         <MemberPage>
-            <table>
+            <h3>회원 관리</h3>
+            <table style={{width : '700px'}}>
             <thead>
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Role</th>
-                <th className="actions">Actions</th>
+                <th style={{width : '20%'}}>ID</th>
+                <th style={{width : '50%'}}>Username</th>
+                <th style={{width : '20%'}}>Role</th>
+                <th style={{width : '10%'}} className="actions">Actions</th>
             </tr>
             </thead>
             <tbody>
@@ -59,11 +70,15 @@ function MemberManage() {
              (
                 userList.map((user) => (
                     <tr key={user.userId}>
-                        <td>{user.userId}</td>
-                        <td>{user.username}</td>
-                        <td>{user.role}</td>
-                        <td className="actions">
-                            <button>탈퇴</button>
+                        <td style={{textAlign : 'center'}}>{user.userId}</td>
+                        <td style={{textAlign : 'center'}}>{user.username}</td>
+                        <td style={{textAlign : 'center'}}>{user.role}</td>
+                        <td style={{textAlign : 'center'}} className="actions">
+                            <button onClick={() => {
+                                if(window.confirm("탈퇴를 진행하겠습니까?")) {
+                                    deleteUser(user.userId);
+                                }
+                                }}>탈퇴</button>
                         </td>
                     </tr>
                 ) )
